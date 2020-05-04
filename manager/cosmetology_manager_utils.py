1e09eaaedb26593c959237f69797775317a255c3
yevhen dzyuba <evgendzyuba7@gmail.com>
2020-05-04 10:32:37 +0300

    added fixed files

--- manager/cosmetology_manager_utils.py
+++ manager/cosmetology_manager_utils.py
@@ -6,16 +6,18 @@ from classes import cosmetology_build
 from manager import cosmetology_manager
 
 
-
 class CosmetologyManagerUtils:
     cosmetology_builds_list = [mask(1, "Ukraine", 123, 340, 423), scrub(3, "France", 345, 456, 123),
-                                                 powder(2, "Spain", 56, 546, 243)]
+                               powder(2, "Spain", 56, 546, 243)]
 
     @staticmethod
-    def sort_by_amount_in_packet(reverse = None):
+    def sort_by_amount_in_packet(reverse=None):
         """
         >>> sport_build_manager_object_utils.sort_by_amount_in_packet(reverse = True)
-        
-        :param reverse:
-        :return:
+        AppoinmentFor:1 ProduceCountry:Ukraine CapacityInMl:123 PriceInUAH:340 AmountInPacket:423 PeriodTimeOfUse:23 ShadeOfShine:White NaturalComponents:NaturalComponents.COFFE
+
+        >>> sport_build_manager_object_utils.sort_by_amount_in_packet(reverse = False)
+        AppoinmentFor:3 ProduceCountry:France CapacityInMl:345 PriceInUAH:456 AmountInPacket:123 PeriodTimeOfUse:34 ShadeOfShine:Black NaturalComponents:NaturalComponents.SUGAR
+
+
         """
